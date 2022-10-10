d7cbf093f3e62ffbf29d35773558726bb215e979
Alvaro Alvarez <f.alvarezarg@gmail.com>
2022-10-10 11:51:31 -0300

    subiendo models

--- AppEntrega1/models.py
+++ AppEntrega1/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Peliculas(models.Model):
+    nombre = models.CharField (max_length=30)
+    genero = models.CharField (max_length=30)
+    estreno = models.CharField (max_length=30)
+    
+
+class Series_Accion (models.Model):
+   nombre = models.CharField (max_length=30)
+   genero = models.CharField (max_length=30)
+   temporadas = models.CharField (max_length=30)
+   capitulos = models.CharField (max_length=30)
+    
+class Generos (models.Model):
+    pass
